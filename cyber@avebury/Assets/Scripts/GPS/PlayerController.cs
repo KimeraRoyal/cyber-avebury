781b0e64a47811a9721c4e3b639b95942ab2c524
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2025-08-17 00:37:37 +0100

    GPS Movement

--- cyber@avebury/Assets/Scripts/GPS/PlayerController.cs
+++ cyber@avebury/Assets/Scripts/GPS/PlayerController.cs
@@ -1,10 +1,17 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Android;
 
 namespace CyberAvebury
 {
     public class PlayerController : MonoBehaviour
     {
         private GPS m_gps;
+
+        [SerializeField] private float m_accuracy = 10.0f;
+        [SerializeField] private float m_updateDistance = 10.0f;
+
+        private double m_lastUpdateTime;
 
 #if DEBUG
         [SerializeField] private LatLng m_debugPosition;
@@ -23,11 +30,64 @@ namespace CyberAvebury
 #if DEBUG
             transform.position = m_gps.GetScenePosition(m_debugPosition);
 #endif
+            StartCoroutine(GpsMovement());
         }
 
         private void Update()
         {
             KeyboardMovement();
+        }
+
+        private IEnumerator GpsMovement()
+        {
+            if (Application.isEditor) { yield break; }
+
+            yield return AuthorizePermission(Permission.FineLocation);
+            if (!Input.location.isEnabledByUser)
+            {
+                Debug.LogError("User has location services disabled.");
+                yield break;
+            }
+            
+            Input.location.Start(m_accuracy, m_updateDistance);
+            var maxWait = 20;
+            while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+            {
+                yield return new WaitForSeconds(1);
+                maxWait--;
+            }
+            
+            if (maxWait < 1)
+            {
+                Debug.LogError("Location services timed out.");
+                yield break;
+            }
+            
+            if (Input.location.status == LocationServiceStatus.Failed)
+            {
+                Debug.LogError("Location services failed to initialise.");
+                yield break;
+            }
+            
+            while (isActiveAndEnabled)
+            {
+                var gpsInfo = Input.location.lastData;
+                if (gpsInfo.timestamp > m_lastUpdateTime)
+                {
+                    m_lastUpdateTime = gpsInfo.timestamp;
+                    var location = new LatLng(gpsInfo.latitude, gpsInfo.longitude);
+                    MoveToCoordinates(location);
+                }
+
+                yield return null;
+            }
+            Input.location.Stop();
+        }
+
+        private void MoveToCoordinates(LatLng _coordinates)
+        {
+            var position = m_gps.GetScenePosition(_coordinates);
+            transform.position = position;
         }
 
         private void KeyboardMovement()
@@ -38,6 +98,20 @@ namespace CyberAvebury
             transform.position += movement * (m_movementSpeed * Time.deltaTime);
             transform.eulerAngles += rotation * (m_rotationSpeed * Time.deltaTime);
 #endif
+        }
+
+        private IEnumerator AuthorizePermission(string _permission)
+        {
+#if UNITY_ANDROID
+            if (Permission.HasUserAuthorizedPermission(_permission)) { yield break; }
+            
+            Permission.RequestUserPermission(_permission);
+            while (!Permission.HasUserAuthorizedPermission(_permission))
+            {
+                yield return new WaitForSeconds(1.0f);
+            }
+#endif
+            yield return null;
         }
     }
 }
