877aba5192c390aeab3872cab04202f0c07b6992
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2025-08-16 14:15:33 +0100

    Added GPS functionality

---
+++ cyber@avebury/Assets/Scripts/GPS/GPS.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace CyberAvebury
+{
+    public class GPS : MonoBehaviour
+    {
+        [SerializeField] private LatLng m_origin;
+        [SerializeField] private float m_mapScale = 1.0f;
+
+        private void Awake()
+        {
+            GPSEncoder.SetLocalOrigin(m_origin);
+        }
+        
+        public T PlaceObjectAt<T>(T _prefab, LatLng _coordinates, Quaternion _rotation) where T : Object
+            => Instantiate(_prefab, GetScenePosition(_coordinates), _rotation, transform);
+
+        public Vector3 GetScenePosition(LatLng _coordinates)
+            => MetersToScene(GPSEncoder.GPSToUCS(_coordinates));
+
+        public LatLng GetCoordinates(Vector3 _position)
+            => GPSEncoder.USCToGPS(SceneToMeters(_position));
+
+        #region Unit Conversion
+        public float SceneToMeters(float _sceneUnits)
+            => _sceneUnits / m_mapScale;
+
+        public Vector2 SceneToMeters(Vector2 _sceneUnits)
+            => _sceneUnits / m_mapScale;
+
+        public Vector3 SceneToMeters(Vector3 _sceneUnits)
+            => _sceneUnits / m_mapScale;
+
+        public float MetersToScene(float _unitInMeters)
+            => _unitInMeters * m_mapScale;
+
+        public Vector2 MetersToScene(Vector2 _unitInMeters)
+            => _unitInMeters * m_mapScale;
+
+        public Vector3 MetersToScene(Vector3 _unitInMeters)
+            => _unitInMeters * m_mapScale;
+        #endregion
+    }
+}
---
+++ cyber@avebury/Assets/Scripts/GPS/LatLng.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace CyberAvebury
+{
+    [Serializable]
+    public struct LatLng
+    {
+        [SerializeField] private double m_latitude;
+        [SerializeField] private double m_longitude;
+        
+        public double Latitude { get => m_latitude; set => m_latitude = value; }
+        public double Longitude { get => m_longitude; set => m_longitude = value; }
+
+        public LatLng(double _latitude, double _longitude)
+        {
+            m_latitude = _latitude;
+            m_longitude = _longitude;
+        }
+            
+        public static implicit operator Vector2(LatLng _coordinates)
+            => new((float) _coordinates.Latitude, (float) _coordinates.Longitude);
+
+        public static implicit operator LatLng(Vector2 _position)
+            => new(_position.x, _position.y);
+    }
+}
