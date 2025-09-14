f05470d30a0ecd738c95f6a1c704560677d1f062
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2025-09-14 21:16:37 +0100

    Added a bit of velocity to rotating

--- cyber@avebury/Assets/Scripts/Camera/CameraGestures.cs
+++ cyber@avebury/Assets/Scripts/Camera/CameraGestures.cs
@@ -16,7 +16,9 @@ namespace CyberAvebury
         [SerializeField] private float m_zoomDamping = 1.0f;
 
         [Range(-180.0f, 180.0f)] [SerializeField] private float m_rotation;
-        [SerializeField] private float m_rotateSpeed = 180.0f;
+        [SerializeField] private AnimationCurve m_zoomRotationSpeedCurve = AnimationCurve.Linear(0.0f, 45.0f, 1.0f, 90.0f);
+        [SerializeField] private AnimationCurve m_zoomMaxRotationSpeedCurve = AnimationCurve.Linear(0.0f, 30.0f, 1.0f, 45.0f);
+        [SerializeField] private AnimationCurve m_zoomRotationDampingCurve = AnimationCurve.Linear(0.0f, 0.1f, 1.0f, 0.2f);
 
         [SerializeField] private float m_scrollSpeed = 1.0f;
 
@@ -25,6 +27,9 @@ namespace CyberAvebury
 
         private float m_currentZoom;
         private float m_zoomVelocity;
+
+        private float m_currentBias;
+        private float m_biasVelocity;
 
         private void Awake()
         {
@@ -42,15 +47,19 @@ namespace CyberAvebury
             
             m_currentZoom = Mathf.SmoothDamp(m_currentZoom, m_zoom, ref m_zoomVelocity, m_zoomDamping);
             m_transposer.m_FollowOffset = new Vector3(0.0f, m_zoomYCurve.Evaluate(m_currentZoom), m_zoomZCurve.Evaluate(m_currentZoom));
+
+            m_currentBias = Mathf.SmoothDamp(m_currentBias, 0.0f, ref m_biasVelocity, m_zoomRotationDampingCurve.Evaluate(m_zoom));
+            var bias = m_transposer.m_Heading.m_Bias;
+            bias += m_currentBias * m_zoomRotationSpeedCurve.Evaluate(m_zoom);
+            if (bias < 180.0f) { bias += 360.0f; }
+            if (bias > 180.0f) { bias -= 360.0f; }
+            m_transposer.m_Heading.m_Bias = bias;
         }
 
         private void RotateCamera(float _delta)
         {
-            var bias = m_transposer.m_Heading.m_Bias;
-            bias += _delta * m_rotateSpeed;
-            if (bias < 180.0f) { bias += 360.0f; }
-            if (bias > 180.0f) { bias -= 360.0f; }
-            m_transposer.m_Heading.m_Bias = bias;
+            var maxSpeed = m_zoomMaxRotationSpeedCurve.Evaluate(m_zoom);
+            m_currentBias = Mathf.Clamp(m_currentBias + _delta, -maxSpeed, maxSpeed);
         }
 
         private void Zoom(float _delta)
