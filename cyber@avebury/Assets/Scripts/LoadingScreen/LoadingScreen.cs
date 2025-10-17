2fcf74b4272555d8b68b8daa1c5c6704cdeb56dd
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2025-10-17 12:02:53 +0100

    Fixed loading screen glitching

--- cyber@avebury/Assets/Scripts/LoadingScreen/LoadingScreen.cs
+++ cyber@avebury/Assets/Scripts/LoadingScreen/LoadingScreen.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -21,8 +22,13 @@ namespace CyberAvebury.LoadingScreen
 
         private Animator m_animator;
 
+        [SerializeField] private Animator m_tilesPrefab;
+        private Animator m_tiles;
+
         [SerializeField] private bool m_opened;
         private bool m_showing;
+
+        private bool m_isGlitched;
 
         public bool IsOpened => m_opened;
         public bool IsShowing => m_showing;
@@ -44,7 +50,17 @@ namespace CyberAvebury.LoadingScreen
 
             DontDestroyOnLoad(gameObject);
             Instance = this;
+            
+            SpawnTiles();
         }
+
+        [Button("Show")]
+        public void Show()
+            => ShowScreen();
+
+        [Button("Long Show")]
+        public void LongShow()
+            => ShowScreen(4.0f);
 
         public bool ShowScreen(float _duration = 1.0f, Action OnLoad = null)
         {
@@ -53,8 +69,20 @@ namespace CyberAvebury.LoadingScreen
             return true;
         }
 
+        [Button("Glitch")]
+        public void Glitch()
+            => GlitchScreen(true);
+
+        [Button("Unglitch")]
+        public void Unglitch()
+            => GlitchScreen(false);
+        
         public void GlitchScreen(bool _glitch)
-            => m_animator.SetBool("Glitching", _glitch);
+        {
+            m_isGlitched = _glitch;
+            m_animator.SetBool("Glitching", _glitch);
+            m_tiles.SetBool("Glitching", _glitch);
+        }
 
         private IEnumerator Present(float _duration, Action OnLoad)
         {
@@ -70,6 +98,31 @@ namespace CyberAvebury.LoadingScreen
             m_animator.SetBool("Show", false);
             m_showing = false;
             OnHide?.Invoke();
+        }
+
+        public void ShowTiles()
+        {
+            m_tiles.gameObject.SetActive(true);
+        }
+
+        public void HideTiles()
+        {
+            if (m_isGlitched)
+            {
+                // Fix for visual bug with loading screen after unglitching
+                Destroy(m_tiles.gameObject);
+                SpawnTiles();
+                GlitchScreen(false);
+            }
+            
+            m_tiles.gameObject.SetActive(false);
+        }
+
+        private void SpawnTiles()
+        {
+            m_tiles = Instantiate(m_tilesPrefab, transform);
+            m_tiles.name = m_tilesPrefab.name;
+            m_tiles.gameObject.SetActive(false);
         }
     }
 }
